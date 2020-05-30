e559b3f454efb7f63a07536444458718bc31ab2b
BurnieChen <chen0317029@gmail.com>
2020-05-30 17:47:49 +0800

    add handleMessage, callSendAPI

--- index.js
+++ index.js
@@ -17,23 +17,34 @@ app.post('/profile', (req, res) => {
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
   
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+    
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);        
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+        } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+        }
   
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-  });
+});
 
 // Adds support for GET requests to our webhook
 app.get('/profile', (req, res) => {
@@ -60,3 +71,31 @@ app.get('/profile', (req, res) => {
       }
     }
   });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    if (received_message.text) {
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+}
