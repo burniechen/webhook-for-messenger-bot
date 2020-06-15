365675f9241c9379fa777f2abf374da1d296e8bd
nchuweb <ppw11806@bcaoo.com>
2020-06-15 17:03:10 +0800

    axios v3

--- index.js
+++ index.js
@@ -102,9 +102,8 @@ function handleMessage(sender_psid, received_message) {
 			image: attachment_url
 		})
 		.then(function (res) {
-			console.log(res.data.data['text']);
 			response = res.data.data['text']
-			send = False;
+			send = false;
 			callSendAPI(sender_psid, response); 
 		})
 		.catch(function (error) {
