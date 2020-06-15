4511b3e35f776f188b3b36fcc1690de89f846455
nchuweb <ppw11806@bcaoo.com>
2020-06-15 11:04:51 +0800

    reply v8

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ function handleMessage(sender_psid, received_message) {
 
 		reply_key = getKeyByValue(reply.init_msg, msg_text);
 		console.log("get key: "+reply_key);
+		console.log("reply: "+reply.init_reply.reply_key);
 
 		if (reply_key) response = reply.init_reply.reply_key;
 		else response = {"text": "請等候回復"};
