e2f7887cc5ce5cd5e551e09ead96762ad9579dee
nchuweb <ppw11806@bcaoo.com>
2020-06-15 16:56:35 +0800

    imstall axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as reply from "./reply.js";
 import express from 'express';
 import bodyParser from 'body-parser';
 import request from 'request';
+import axios from 'axios'
 'use strict';
 
 // Imports dependencies and set up http server
@@ -96,32 +97,41 @@ function handleMessage(sender_psid, received_message) {
 	else if (received_message.attachments) {
   
         // Gets the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
+		let attachment_url = received_message.attachments[0].payload.url;
+		axios.post('https://5e8643c72f31.ngrok.io/rest/image', {
+			image: attachment_url
+		})
+		.then(function (response) {
+			console.log(response.data);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+        // response = {
+        //     "attachment": {
+        //         "type": "template",
+        //         "payload": {
+        //             "template_type": "generic",
+        //             "elements": [{
+        //                 "title": "Is this the right picture?",
+        //                 "subtitle": "Tap a button to answer.",
+        //                 "image_url": attachment_url,
+        //                 "buttons": [
+        //                     {
+        //                         "type": "postback",
+        //                         "title": "Yes!",
+        //                         "payload": "yes",
+        //                     },
+        //                     {
+        //                         "type": "postback",
+        //                         "title": "No!",
+        //                         "payload": "no",
+        //                     }
+        //                 ],
+        //             }]
+        //         }
+        //     }
+        // }
     }
     
     // Sends the response message
