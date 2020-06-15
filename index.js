13a54880047b2491ed62d08bd440da1dab36397c
nchuweb <ppw11806@bcaoo.com>
2020-06-15 17:11:12 +0800

    axios v5

--- index.js
+++ index.js
@@ -98,11 +98,14 @@ function handleMessage(sender_psid, received_message) {
   
         // Gets the URL of the message attachment
 		let attachment_url = received_message.attachments[0].payload.url;
+		console.log(attachment_url)
 		axios.post('https://5e8643c72f31.ngrok.io/rest/image', {
 			image: attachment_url
 		})
 		.then(function (res) {
-			response = res.data.data
+			response = {
+				"text": res.data.data['text']
+			}
 			send = false;
 			callSendAPI(sender_psid, response); 
 		})
