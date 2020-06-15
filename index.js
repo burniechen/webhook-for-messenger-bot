614a072eaa2af1d46248697956bbc579542b07b1
BurnieChen <chen0317029@gmail.com>
2020-06-15 22:48:35 +0800

    add env v4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express().use(bodyParser.json()); // creates express http server
 //.env
 dotenv.config();
 const api = process.env.API_URL;
-const PAGE_ACCESS_TOKEN = process.env.API_URL;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 // Sets server port and logs message on success
